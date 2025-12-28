060c7efeda3cfa1337afd185114bee0dc30f4179
akoidan <deathangel908@gmail.com>
2025-12-28 18:07:13 +0100

    dd

--- src/main/kotlin/Solution.kt
+++ src/main/kotlin/Solution.kt
@@ -7,16 +7,40 @@ data class WindowItem(
 
 class Solution {
     fun maxSlidingWindow(nums: IntArray, k: Int): IntArray {
+        if (k < 1) {
+            throw IllegalArgumentException("Illegal sliding window size")
+        }
+        if (nums.size < k) {
+            throw IllegalArgumentException("Array should be greater sliding window")
+        }
+        if (k == 1) {
+            return nums
+        }
 
+        val result = ArrayList<Int>() // nums.size - k + 1
         val deq = ArrayDeque<WindowItem>()
         deq.add(WindowItem(0, nums[0]))
-        for (i in 1 until k) {
+        for (i in 1 until k - 1) {
             if (nums[i] >= deq[0].value) {
                 deq.clear()
-                deq.add(WindowItem(i, nums[i]))
-            } else if (i - deq[0].index > k){
-
             }
+            deq.add(WindowItem(i, nums[i]))
         }
+
+        for (i in k - 1 until nums.size) {
+            if (deq.size >= k || deq.first().index < i - k + 1) {
+                deq.removeFirst()
+            }
+            if (nums[i] >= deq[0].value) {
+                deq.clear()
+            }
+            while (deq.isNotEmpty() && deq.last().value <= nums[i]) {
+                deq.removeLast()
+            }
+            deq.add(WindowItem(i, nums[i]))
+            result.add(deq[0].value)
+        }
+
+        return result.toIntArray()
     }
 }
--- src/test/kotlin/org/example/SolutionTest.kt
+++ src/test/kotlin/org/example/SolutionTest.kt
@@ -1,12 +1,44 @@
 package org.example
 
 import kotlin.test.Test
+import kotlin.test.assertContentEquals
 import kotlin.test.assertEquals
 
 class SolutionTest {
     @Test
-    fun maxSlidingWindow_returnsIntArrayOfSizeOne() {
-        val result = Solution().maxSlidingWindow(intArrayOf(1, 2, 3), 3)
-        assertEquals(1, result.size)
+    fun maxSlidingWindow_u1() {
+        val result = Solution().maxSlidingWindow(intArrayOf(1,2,1,0,4,2,6), 3)
+        assertContentEquals(result, intArrayOf(2,2,4,4,6))
+    }
+
+
+    @Test
+    fun maxSlidingWindow_u2() {
+        val result = Solution().maxSlidingWindow(intArrayOf(1,3,-1,-3,5,3,6,7), 3)
+        assertContentEquals(result, intArrayOf(3,3,5,5,6,7))
+    }
+
+    @Test
+    fun maxSlidingWindow_u3() {
+        val result = Solution().maxSlidingWindow(intArrayOf(7, 2, 4), 2)
+        assertContentEquals(result, intArrayOf(7, 4))
+    }
+
+    @Test
+    fun maxSlidingWindow_s1() {
+        val result = Solution().maxSlidingWindow(intArrayOf(1), 1)
+        assertContentEquals(result, intArrayOf(1))
+    }
+
+    @Test
+    fun maxSlidingWindow_decreasing() {
+        val result = Solution().maxSlidingWindow(intArrayOf(1,2,1,0,4,2,6), 3)
+        assertContentEquals(result, intArrayOf(2,2,4,4,6))
+    }
+
+    @Test
+    fun maxSlidingWindow_decreasing2() {
+        val result = Solution().maxSlidingWindow(intArrayOf(1,3,1,2,0,5), 3)
+        assertContentEquals(result, intArrayOf(3,3,2,5))
     }
 }
